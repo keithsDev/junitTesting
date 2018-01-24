bd1a99ffee9ce70e3e6456ba4e69acbf4e750dc7
keithsDev <35673542+keithsDev@users.noreply.github.com>
2018-01-24 10:52:43 +0000

    Update TestStrings.java

--- JUntTestSample/Testing/TestStrings.java
+++ JUntTestSample/Testing/TestStrings.java
@@ -11,7 +11,7 @@ public class TestStrings {
 
 		myClass test = new myClass();
 		String res = test.addStrings("Hello", "world" );
-		assertEquals("Helloworld", res);
+		assertEquals("HelloWorld", res);
 				
 	}
 
